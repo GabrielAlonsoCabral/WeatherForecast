2f4db94d655419692ac1583ad4a5dcc7cc9cbe33
Gabriel Alonso <gabriel.alonso2589@gmail.com>
2024-04-11 21:40:38 -0300

    feat: initial structure

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast/{id}")]
+    public IEnumerable<WeatherForecast> Get(long id)
+    {
+        _logger.LogDebug($"Request made {id}");
+        Console.WriteLine($"Id on parameter {id}");
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost]
+    public bool CreateWeatherForecast(WeatherForecast weatherForecast){
+        Console.WriteLine($"C: {weatherForecast.TemperatureC} F: {weatherForecast.TemperatureF}");
+        return true;
+    }
+
+}
